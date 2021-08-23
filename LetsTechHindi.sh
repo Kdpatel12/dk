94439f1b9d2b7f8d3a3e6b0394cd58e7b03d170d
Kdpatel12 <89411012+Kdpatel12@users.noreply.github.com>
2021-08-23 23:07:42 +0530

    Add files via upload

---
+++ LetsTechHindi.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by LETS TECH HINDI now start"
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#####################################"
+echo   " we are Learner"               
+ echo   " NOT BAD HACKERS "                                                                    
+echo   "####################################"
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "."
+
+apt-get update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+echo  "#####################################"
+echo   " u can now hack anything      "               
+ echo   " completing wait 3 2 1 "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+echo  "#####################################"
+echo   "anynomous "               
+ echo   " join us on INSTAGRAM ID hritik14619 "                                                                   echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo " "                                                                   
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+                                                            echo   " "
+                                                            
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+apt install tor -y
+apt install w3m -y
+apt install cmatrix -y
+echo  "##########LETS#TECH#HINDI###########"
+echo   " Join LET'S TECH HINDI YOUTUBE FAMILY "               
+                                                                  echo   "#######LETS#TECH#HINDI##############"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo  "########LETS#TECH#HINDI############"
+               
+echo   " processing wait......
+"                                                                    
+echo   "##########LETS#TECH#HINDI#############"
+
+echo " If You Connected To Internet Then u downloaded success subscribe us LetS Tech Hindi ...Dont Learn To Hack, Hack To Learn "
+
+echo " ??? Now SUBSCRIBE LETS TECH HINDI and Enjoy 
+??? "
+
+exit
